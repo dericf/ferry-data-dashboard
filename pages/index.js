70578abf6e9ca82acb80401fcb664070a26d51a6
deric92 <dericgf@gmail.com>
2021-01-24 21:28:51 -0700

    Fix the layout + improve code to download and refresh table

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 /*
-  * https://www.npmjs.com/package/react-csv
-*/
+ * https://www.npmjs.com/package/react-csv
+ */
 import Head from "next/head";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { CSVLink, CSVDownload } from "react-csv";
@@ -23,33 +23,29 @@ export default function Dashboard({ data, loading }) {
 
   // let formatted_date_string = new Date().toUTCString().replace(/ /g, "_").replace(/,/g, "").replace(/:/g, "_")
 
-
   const refreshData = async () => {
     const resp = await fetch("/api/get-limited-data");
     const json = await resp.json();
-    
-    setTableData(json.capacity_data)
+
+    setTableData(json.capacity_data);
+  };
+
+  function timeout(delay) {
+    return new Promise((res) => setTimeout(res, delay));
   }
 
-  
-  const download = async (event, done) => {
+  const download = async () => {
+    // Fetch the entire dataset
     const resp = await fetch("/api/downloadcsv");
     const json = await resp.json();
-    console.log("Response: ", json)
-    setCsvData(json.capacity_data)
-    setTimeout(() => {
-      done(); // REQUIRED to invoke the logic of component
-      
-    }, 2000);
-  }
+    setCsvData(json.capacity_data);
+  };
 
   useEffect(() => {
-    (async () =>{
-      refreshData()
-    }
-    )()
-    
-  }, [])
+    (async () => {
+      refreshData();
+    })();
+  }, []);
 
   return (
     <div
@@ -67,11 +63,13 @@ export default function Dashboard({ data, loading }) {
 
       <main
         style={{
-          minHeight: "50vh",
+          minHeight: "100vh",
+          maxHeight: "100vh",
+          overflow: "scroll",
           width: "100vw",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center",
+          justifyContent: "start",
           alignItems: "center",
           backgroundColor: "#303030",
           color: "white",
@@ -82,28 +80,26 @@ export default function Dashboard({ data, loading }) {
           <h1 style={{ textAlign: "center" }}>Capacity Data</h1>
           {/* <button className="button" onClick={downloadData}>Download</button> */}
           {!loading && (
-            <CSVLink data={csvData} headers={headers} asyncOnClick={true} filename={`ferry-data.csv`} onClick={(event, done)=>(download(event, done))}>
-              <button className="download-button">
-                Download All Data as CSV
-              </button>
-            </CSVLink>
-
-          )}
-            <button className="download-button" onClick={refreshData}>
-              Refresh Table
+            <button className="download-button" onClick={download}>
+              Download All Data as CSV
             </button>
+          )}
+          <button className="download-button" onClick={refreshData}>
+            Refresh Table
+          </button>
         </div>
-        <span style={{textAlign:"left", marginBottom:"0.5rem"}}>Note: This table only displays the 1000 newest entries. Download button will download entire dataset.</span>
-        {!tableData && (
-          <span>Data is Loading...</span>
-        )}
+        <span style={{ textAlign: "left", marginBottom: "0.5rem" }}>
+          Note: This table only displays the 1000 newest entries. Download
+          button will download entire dataset.
+        </span>
+        {!tableData && <span>Data is Loading...</span>}
         {tableData && (
           <table>
             <thead>
               <tr>
                 <th>Id</th>
                 <th>Crossing Name</th>
-                <th>Time of Sailing</th>
+                <th colSpan="2">Date/Time of Sailing</th>
                 <th className="center">Capacity (% Available)</th>
                 <th colSpan="2">Date (yyyy-mm-dd) / Time Recorded</th>
               </tr>
@@ -113,18 +109,23 @@ export default function Dashboard({ data, loading }) {
                 <tr key={row.id}>
                   <td className="right">{row.id}</td>
                   <td className="left">{row.crossing_name}</td>
+                  <td className="right">{row.date_of_sailing}</td>
                   <td className="right">{row.time_of_sailing}</td>
                   <td className="center">{row.percent_available} %</td>
-                  <td className="right">
-                    {row.date_recorded}
-                  </td>
-                  <td className="right">
-                    {row.time_recorded}
-                  </td>
+                  <td className="right">{row.date_recorded}</td>
+                  <td className="right">{row.time_recorded}</td>
                 </tr>
               ))}
             </tbody>
           </table>
+        )}
+        {csvData.length > 0 && (
+          <CSVDownload
+            data={csvData}
+            headers={headers}
+            target="_blank"
+            filename={`ferry-data.csv`}
+          ></CSVDownload>
         )}
       </main>
     </div>
