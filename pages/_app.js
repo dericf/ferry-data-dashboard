5cfebc440f7b2167f591374f8c79f5c6731035bf
deric92 <dericgf@gmail.com>
2021-01-24 18:33:00 -0700

    Add basic css styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../styles/utility.css'
+import '../styles/tables.css'
+import '../styles/buttons.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
