6d5b23e3aaac2c6bcb3d9be04d6c8e363bbdc265
deric92 <dericgf@gmail.com>
2021-01-25 18:42:34 -0700

    Fix date/time formats

--- utilities/dates.js
+++ utilities/dates.js
@@ -1,9 +1,9 @@
 import moment from 'moment'
 
 export const date_format = (input) => {
-	return moment(input).format('ddd, MMM Do YYYY z')
+	return moment(input).format('ddd, MMM Do YYYY')
 }
 
 export const time_format = (input) => {
-	return moment(input).format('h:mm:ss A Z')
+	return moment(input).format('h:mm A (Z)')
 }
