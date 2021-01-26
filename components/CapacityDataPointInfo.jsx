832b3722a5a80209bbb92cd1e494bfcead287250
deric92 <dericgf@gmail.com>
2021-01-25 18:55:00 -0700

    Minor text fixes on info modal

--- components/CapacityDataPointInfo.jsx
+++ components/CapacityDataPointInfo.jsx
@@ -7,9 +7,9 @@ export default function CapacityDataPointInfo({datapoint}) {
 	
 	return (
 		<div className="">
-			<h1 class="text-center">{datapoint.crossing_name} - (ID: {datapoint.id})</h1>
-			<div className="flex flex-col justify-center align-start" style={{flexWrap: 0}}>
-			<p>Sailing Time: <strong>{ date_format(datapoint.date_recorded)} at {time_format(datapoint.date_of_sailing + " " + datapoint.time_of_sailing)}</strong></p>
+			<h2 class="text-center">{datapoint.crossing_name} - (ID: {datapoint.id})</h2>
+			<div className="flex flex-col justify-center align-start text-left" style={{flexWrap: 0}}>
+				<p>Sailing Time: <strong>{ date_format(datapoint.date_recorded)} at {time_format(datapoint.date_of_sailing + " " + datapoint.time_of_sailing)}</strong></p>
 				<span>Capacity Available: <strong>{datapoint.percent_available} %</strong></span>
 				<p>Datapoint Recorded on <strong> {date_format(datapoint.date_recorded)} at {time_format(datapoint.date_recorded + " " + datapoint.time_recorded)}</strong> </p>
 			</div>
