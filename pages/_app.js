ca18e54d2f1959e6fe0072812bd9e3fdc024fa4d
deric92 <dericgf@gmail.com>
2021-01-25 18:50:53 -0700

    Import new css files

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import '../styles/utility.css'
 import '../styles/tables.css'
 import '../styles/buttons.css'
+import '../styles/modals.css'
+import '../styles/typography.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
