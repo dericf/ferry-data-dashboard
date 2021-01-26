51e37caeb58982eafea525e0e911f5801876d1af
deric92 <dericgf@gmail.com>
2021-01-25 18:42:04 -0700

    Fix Record Info Modal format

--- components/CapacityDataPointInfo.jsx
+++ components/CapacityDataPointInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import moment from 'moment'
 
+import { date_format, time_format } from "../utilities/dates";
+
 export default function CapacityDataPointInfo({datapoint}) {
 	
 	return (
@@ -12,13 +14,13 @@ export default function CapacityDataPointInfo({datapoint}) {
 		<td className="right">{row.date_recorded}</td>
 		<td className="right">{row.time_recorded}</td> */}
 
-			<h1>{datapoint.id} - {datapoint.crossing_name}</h1>
+			<h1>{datapoint.crossing_name} - (ID: {datapoint.id})</h1>
 			{/* <p>{moment("2020-01-01 " + "20:24:17-08").format()}</p> */}
 
-			<div className="flex flex-col jus		tify-center align-center" style={{flexWrap: 0}}>
-			<p>Sailing Time: <strong>{moment(datapoint.date_recorded).format('dddd, MMMM Do YYYY z')} at {moment(datapoint.date_of_sailing + " " + datapoint.time_of_sailing).format('h:mm:ss A')}</strong></p>
-				<span>Capacity Available: {datapoint.percent_available} %</span>
-				<p>Datapoint Recorded on <strong> {moment(datapoint.date_recorded).format('dddd, MMMM Do YYYY z')} at {moment(datapoint.date_recorded + " " + datapoint.time_recorded).format('h:mm:ss A')} (PST)</strong> </p>
+			<div className="flex flex-col justify-center align-start" style={{flexWrap: 0}}>
+			<p>Sailing Time: <strong>{ date_format(datapoint.date_recorded)} at {time_format(datapoint.date_of_sailing + " " + datapoint.time_of_sailing)}</strong></p>
+				<span>Capacity Available: <strong>{datapoint.percent_available} %</strong></span>
+				<p>Datapoint Recorded on <strong> {date_format(datapoint.date_recorded)} at {time_format(datapoint.date_recorded + " " + datapoint.time_recorded)}</strong> </p>
 			</div>
 		</div>
 	)
