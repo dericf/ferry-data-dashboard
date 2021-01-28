e92b52b813aca8b83674640029d26648f3be2b9c
deric92 <dericgf@gmail.com>
2021-01-27 23:27:28 -0700

    Massive overhaul to index.js (still a ways to go)

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,42 @@ import ConfirmModal from "../components/ConfirmModal";
 import InfoModal from "../components/InfoModal";
 import CapacityDataPointInfo from "../components/CapacityDataPointInfo";
 import { date_format, time_format } from "../utilities/dates";
+import DataTable from "../components/DataTable";
+import WorkerPortal from "../components/WorkerPortal";
+import { useToggle } from "../hooks/useToggle";
+import { useInputValue } from "../hooks/useInputValue";
+import { useBasicAuth } from "../hooks/useBasicAuth";
+import DownloadCSVButton from "../components/DownloadCSVButton";
+import { useAlertPopup } from "../hooks/useAlert";
+import AlertPopup from "../components/AlertPopup";
+import { useLoadingScreen } from "../hooks/useLoadingScreen";
+import LoadingBackdrop from "../components/LoadingBackdrop";
 
-export default function Dashboard({ data, loading }) {
-  const [csvData, setCsvData] = useState([]);
+export default function Dashboard({ data }) {
   const [tableData, setTableData] = useState();
   const [terminals, setTerminals] = useState();
   const [selectedTerminal, setSelectedTerminal] = useState();
+  const { loading, setLoading } = useLoadingScreen();
 
-  const headers = [
-    { label: "id", key: "id" },
-    { label: "crossing_name", key: "crossing_name" },
-    { label: "time_of_sailing", key: "time_of_sailing" },
-    { label: "date_recorded", key: "date_recorded" },
-    { label: "time_recorded", key: "time_recorded" },
-    { label: "percent_available", key: "percent_available" },
-    { label: "created_at", key: "created_at" },
-    { label: "updated_at", key: "updated_at" },
-  ];
+  const [open, toggle] = useToggle(null);
+  const { auth, setAuth } = useBasicAuth(true);
+  const { value: pwd } = useInputValue();
+
+  const { alertText, setAlertText, setAlertStatus } = useAlertPopup(
+    "TESTINETG",
+  );
 
   // let formatted_date_string = new Date().toUTCString().replace(/ /g, "_").replace(/,/g, "").replace(/:/g, "_")
 
   const refreshData = async () => {
-    const resp = await fetch("/api/get-limited-data");
+    setLoading(true);
+    const resp = await fetch("/api/get-limited-data", {
+      headers: { "X-AUTH": pwd },
+    });
     const json = await resp.json();
 
     setTableData(json.capacity_data);
+    setLoading(false);
   };
 
   const refreshTerminals = async () => {
@@ -44,38 +55,22 @@ export default function Dashboard({ data, loading }) {
     setTerminals(json.terminal);
   };
 
-  function timeout(delay) {
-    return new Promise((res) => setTimeout(res, delay));
-  }
-
-  const download = async () => {
-    // Fetch the entire dataset
-    const resp = await fetch("/api/downloadcsv");
-    const json = await resp.json();
-    setCsvData(json.capacity_data);
-  };
-
   useEffect(() => {
     (async () => {
       refreshData();
-      refreshTerminals();
+      // refreshTerminals();
     })();
-  }, []);
+  }, [auth]);
 
   useEffect(() => {
     if (terminals) {
       setSelectedTerminal(terminals[0]);
     }
   }, [terminals]);
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <div className="container">
+      {loading && <LoadingBackdrop />}
       <Head>
         <title>Ferry Data Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
@@ -83,106 +78,65 @@ export default function Dashboard({ data, loading }) {
 
       {/* TODO: move css to file */}
       <main
+        className={loading ? "blur" : ""}
         style={{
           minHeight: "100vh",
           maxHeight: "100vh",
           overflow: "scroll",
-          width: "100vw",
           display: "flex",
           flexDirection: "column",
           justifyContent: "start",
           alignItems: "center",
-          backgroundColor: "#303030",
           color: "white",
           padding: "2rem",
         }}
       >
-        <div className="flex flex-row justify-center align-center flex-wrap">
-          <h1 style={{ textAlign: "center" }}>Ferry Capacity Data</h1>
+        <div className="flex flex-row flex-shrink flex-no-grow justify-between align-start flex-wrap">
+          <h1 onClick={toggle} style={{ textAlign: "left" }}>
+            Ferry Capacity Data
+          </h1>
           {/* <button className="button" onClick={downloadData}>Download</button> */}
-          <div className="flex flex-row justify-center-align-center">
-          {!loading && (
-            <button className="download-button" onClick={download}>
-              Download All Data as CSV
-            </button>
+
+          <WorkerPortal></WorkerPortal>
+
+          <DownloadCSVButton />
+
+          {alertText?.length > 0 && (
+            <AlertPopup text={alertText} setText={setAlertText} />
           )}
-          <button className="download-button" onClick={refreshData}>
+        </div>
+        <div className="flex flex-row justify-around align-center flex-wrap my-2">
+          <button
+            disabled={!auth}
+            className="button-secondary align-self-center"
+            onClick={refreshData}
+          >
             Refresh Table
           </button>
-          </div>
-        </div>
-        <div className="flex flex-row" style={{ flexShrink: "0", flexGrow: 1}}>
-        {!tableData ? ( <p>Data is Loading...</p>) : (
-          <p style={{ textAlign: "left", marginBottom: "0.5rem" }}>
-            Note: This table only displays the 100 newest entries. Download
-            button will download entire datet.
-          </p>
-        )}
-          {terminals && (
+
+          <button
+            className="button-secondary align-self-center mx-2"
+            onClick={() => setAlertText("Testing this alert out")}
+          >
+            Settings
+          </button>
+          {tableData && auth && (
+            <p className="text-left">
+              Note: This table only displays the 100 newest entries. Download
+              button will download entire datet.
+            </p>
+          )}
+          {/* {terminals && (
             <ActionBar
               terminals={terminals}
               selectedTerminal={selectedTerminal}
               onChange={(e) => setSelectedTerminal(e.target.value)}
             />
-          )}
+          )} */}
         </div>
-        
-        {tableData && (
-          <div style={{overflowX: "auto", maxWidth: "95vw"}}>
-          <table >
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Crossing Name</th>
-                <th colSpan="2">Date/Time of Sailing</th>
-                <th className="center">Capacity (Available)</th>
-                <th colSpan="2">Date / Time Recorded</th>
-                <th className="center">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableData.map((row) => (
-                <tr key={row.id}>
-                  <td className="right">{row.id}</td>
-                  <td className="left">{row.crossing_name}</td>
-                  <td className="right" style={{ borderRight: "none" }}>
-                    {date_format(row.date_of_sailing)}
-                  </td>
-                  <td className="right" style={{ borderLeft: "none" }}>
-                    {time_format(
-                      row.date_of_sailing + " " + row.time_of_sailing,
-                    )}
-                  </td>
-                  <td className="center">{row.percent_available} %</td>
-                  <td className="right">{date_format(row.date_recorded)}</td>
-                  <td className="right">
-                    {time_format(row.date_recorded + " " + row.time_recorded)}{" "}
-                  </td>
-                  <td className="right">
-                    <div
-                      className="flex flex-row justify-center align-center"
-                      style={{ width: "6rem" }}
-                    >
-                      {/* <ConfirmModal message={`Are you sure you want to delete ${row.crossing_name}`} onConfirm={() => (console.log("CONGIMED"))} /> */}
-                      <InfoModal>
-                        <CapacityDataPointInfo datapoint={row} />
-                      </InfoModal>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          </div>
-        )}
-        {csvData.length > 0 && (
-          <CSVDownload
-            data={csvData}
-            headers={headers}
-            target="_blank"
-            filename={`ferry-data.csv`}
-          ></CSVDownload>
-        )}
+
+        {/* Render the Table */}
+        {auth && <DataTable tableData={tableData}></DataTable>}
       </main>
     </div>
   );
