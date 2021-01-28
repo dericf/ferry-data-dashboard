dc646c039e9af10f0990d445c29584aec8d5a8a2
deric92 <dericgf@gmail.com>
2021-01-27 23:24:37 -0700

    Move lmited_cap query to proper file

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,5 +1,21 @@
 import {gql } from "@apollo/client";
 
+export const GET_LIMITED_CAPACITY_DATA = gql`
+query GetCapacityData {
+  capacity_data(order_by: {id: desc, crossing_name: asc, date_recorded: asc, time_recorded: asc }, limit: 10) {
+    created_at
+    crossing_name
+    date_recorded
+    id
+    percent_available
+    time_of_sailing
+    date_of_sailing
+    time_recorded
+    updated_at
+  }
+}
+`
+
 export const GET_ALL_TERMINALS = gql`query GetTerminals {
 terminal {
     id
