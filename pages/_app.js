fbab15dcdd4e132eba290e799119df3ff3f613ff
deric92 <dericgf@gmail.com>
2021-01-27 23:26:55 -0700

    Import more stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import '../styles/tables.css'
 import '../styles/buttons.css'
 import '../styles/modals.css'
 import '../styles/typography.css'
+import '../styles/alerts.css'
+import '../styles/forms.css'
+import '../styles/layout.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
