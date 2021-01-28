109abee8849a617e122bac60f550dfd93370948f
deric92 <dericgf@gmail.com>
2021-01-27 23:30:18 -0700

    Add a csv_helper.js file

---
+++ utilities/csv_helpers.js
@@ -0,0 +1,10 @@
+export const headers = [
+	{ label: "id", key: "id" },
+	{ label: "crossing_name", key: "crossing_name" },
+	{ label: "time_of_sailing", key: "time_of_sailing" },
+	{ label: "date_recorded", key: "date_recorded" },
+	{ label: "time_recorded", key: "time_recorded" },
+	{ label: "percent_available", key: "percent_available" },
+	{ label: "created_at", key: "created_at" },
+	{ label: "updated_at", key: "updated_at" },
+];
