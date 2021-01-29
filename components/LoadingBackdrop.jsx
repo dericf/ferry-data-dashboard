b277d12f20a3b9ff983ec528c4e2411949b28ba0
deric92 <dericgf@gmail.com>
2021-01-28 18:25:43 -0700

    Fix memory link for loading screen (forgot to clear timeout on unmount)

--- components/LoadingBackdrop.jsx
+++ components/LoadingBackdrop.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 export default function LoadingBackdrop({message}) {
 	const [dotCount, setDotCount] = useState(['.'])
 	useEffect(() => {
-		setTimeout(() => {
+		let timeoutId = setTimeout(() => {
 			if (dotCount.length < 10) {
 				setDotCount([...dotCount, '.'])
 				
@@ -11,12 +11,15 @@ export default function LoadingBackdrop({message}) {
 				setDotCount(['.'])
 			}
 		}, 200);
+		return () => (
+			clearTimeout(timeoutId)
+		)
 	}, [dotCount])
 
 	return (
 		<div className="loading">
 			<div>{message && (<h2>{message}</h2>)}</div>
-			<div><h3>{dotCount.map(dot => <span>{dot}</span>)}</h3></div>
+			<div><h3>{dotCount.map((dot, index) => <span key={dot+index}>{dot}</span>)}</h3></div>
 		</div>
 	)
 }
