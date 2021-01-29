9176881889d39229a91168de6e6f65708943a6fb
deric92 <dericgf@gmail.com>
2021-01-28 17:17:10 -0700

    Implemented proper useAlert custom hook

--- hooks/useAlert.js
+++ hooks/useAlert.js
@@ -1,11 +1,69 @@
-import { useState } from "react";
+import { useState, useContext, createContext } from "react";
 
-export const useAlertPopup = (initialValue=null) => {
-	const [alertText, setAlertText] = useState(initialValue);
-	const [alertStatus, setAlertStatus] = useState("info");
-   return {
-     alertText,
-		 setAlertText,
-		 setAlertStatus
+
+
+export const initialAlertValue = {
+	alert: {
+		text: "",
+		type: "success",
+		active: false,
+	},
+	setAlert: (_) => {},
+	sendAlert: (_) => {},
+	sendError: (_) => {},
+	resetAlert: () => {},
+}
+
+export const AlertContext = createContext(initialAlertValue)
+
+import React from 'react'
+
+export default function AlertProvider({children}) {
+	
+	const [alert, setAlert] = useState({
+		text: "",
+		type: "success",
+		active: false,
+	})
+
+	const resetAlert = () => {
+		setAlert({
+			text: "",
+			type: "success",
+			active: false,
+		})
+	}
+
+	const sendAlert = (text) => {
+		setAlert({
+			text: text,
+			type: "success",
+			active: true,
+		})
+	}
+
+	const sendError = (text) => {
+		setAlert({
+			text: text,
+			type: "error",
+			active: true,
+		})
+	}
+
+	return (
+		<AlertContext.Provider value={{alert, setAlert, sendAlert, sendError, resetAlert}}>
+		{children}
+		</AlertContext.Provider>
+	)
+}
+
+
+export const useAlert = () => {
+	const {alert, resetAlert, sendAlert, sendError} =  useContext(AlertContext)	
+	return {
+		alert,
+		resetAlert,
+		sendAlert,
+		sendError
   };
 };
