7bc2cfcf110f95353525c3741dea1d12f6aa83a5
deric92 <dericgf@gmail.com>
2021-01-28 17:18:43 -0700

    Major improvements to the Download CSV workflow

--- components/DownloadCSVButton.jsx
+++ components/DownloadCSVButton.jsx
@@ -1,36 +1,45 @@
 import React, { useState } from "react";
 import { CSVDownload } from "react-csv";
+import { useAlert } from "../hooks/useAlert";
 import { useBasicAuth } from "../hooks/useBasicAuth";
 import { useLoadingScreen } from "../hooks/useLoadingScreen";
 import { headers } from "../utilities/csv_helpers";
 
 export default function DownloadCSVButton() {
-  const { auth, setAuth } = useBasicAuth(true);
+  const { isAuthenticated, pwd } = useBasicAuth();
   const [csvData, setCsvData] = useState([]);
-  const { loading, setLoading } = useLoadingScreen();
-
+  const [disableDownload, setDisableDownload] = useState(true);
+  const { setLoading } = useLoadingScreen();
   const [triggerDownload, setTriggerDownload] = useState();
 
-  console.log("auth", auth);
-  console.log("csvData", csvData);
+  const { sendAlert, sendError } = useAlert();
 
   const download = async () => {
     setTriggerDownload(true);
+    setDisableDownload(true);
     // Fetch the entire dataset
   };
 
-  const requestDataset = async () => {
-    setLoading(true);
+  const requestDataset = async (e) => {
+    e.preventDefault();
+    sendAlert("Fetching Full Dataset. Please Wait.");
     // Fetch the dataset
-    const resp = await fetch("/api/downloadcsv");
+    const authInfo = encodeURIComponent(pwd);
+    const resp = await fetch("/api/downloadcsv", {
+      headers: { "X-Password": authInfo },
+    });
     if (resp.status >= 200 && resp.status < 300) {
       const json = await resp.json();
       setCsvData(json.capacity_data);
+      sendAlert("Success! Your Data is now ready to download.");
+      setDisableDownload(false);
+    } else {
+      sendError("Something went wrong. You are not authorized to access the dataset.")
     }
-    setLoading(false);
+    
   };
 
-  if (!auth) {
+  if (!isAuthenticated) {
     return false;
   }
 
@@ -47,7 +56,7 @@ export default function DownloadCSVButton() {
 
       <div className="flex flex-col mx-2 mt-2 justify-between border">
         <button
-          disabled={!auth}
+          disabled={!isAuthenticated}
           className="button-secondary"
           onClick={requestDataset}
         >
@@ -55,7 +64,7 @@ export default function DownloadCSVButton() {
         </button>
 
         <button
-          disabled={!(csvData?.length > 0)}
+          disabled={disableDownload}
           className="button-success mt-2"
           onClick={download}
         >
