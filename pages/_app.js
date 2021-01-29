838c96897ae4a1342b6f9e2a4d190bc625d52162
deric92 <dericgf@gmail.com>
2021-01-28 17:18:27 -0700

    Wrapped _app in Alert and Auth providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
-import '../styles/globals.css'
-import '../styles/utility.css'
-import '../styles/tables.css'
-import '../styles/buttons.css'
-import '../styles/modals.css'
-import '../styles/typography.css'
-import '../styles/alerts.css'
-import '../styles/forms.css'
-import '../styles/layout.css'
+import "../styles/globals.css";
+import "../styles/utility.css";
+import "../styles/tables.css";
+import "../styles/buttons.css";
+import "../styles/modals.css";
+import "../styles/typography.css";
+import "../styles/alerts.css";
+import "../styles/forms.css";
+import "../styles/layout.css";
+import AlertProvider from "../hooks/useAlert";
+import BasicAuthProvider from "../hooks/useBasicAuth";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AlertProvider>
+      <BasicAuthProvider>
+        <Component {...pageProps} />
+      </BasicAuthProvider>
+    </AlertProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
