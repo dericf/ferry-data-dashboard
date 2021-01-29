d4581bb15f117e48960f7b50f6f5913ff077d842
deric92 <dericgf@gmail.com>
2021-01-28 17:57:09 -0700

    Fix CSV headers

--- utilities/csv_helpers.js
+++ utilities/csv_helpers.js
@@ -1,10 +1,10 @@
 export const headers = [
 	{ label: "id", key: "id" },
 	{ label: "crossing_name", key: "crossing_name" },
+	{ label: "date_of_sailing", key: "date_of_sailing" },
 	{ label: "time_of_sailing", key: "time_of_sailing" },
+	{ label: "percent_available", key: "percent_available" },
 	{ label: "date_recorded", key: "date_recorded" },
 	{ label: "time_recorded", key: "time_recorded" },
-	{ label: "percent_available", key: "percent_available" },
-	{ label: "created_at", key: "created_at" },
-	{ label: "updated_at", key: "updated_at" },
+	{ label: "created_at", key: "created_at" }
 ];
