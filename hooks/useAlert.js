9830940b4ad41fd5efeb6df753cb893eba93f7c3
deric92 <dericgf@gmail.com>
2021-02-02 21:46:55 -0700

    Bring ToastNotifications into the useAlerts hook

--- hooks/useAlert.js
+++ hooks/useAlert.js
@@ -1,69 +1,90 @@
 import { useState, useContext, createContext } from "react";
-
+import {useToasts } from 'react-toast-notifications'
 
 
 export const initialAlertValue = {
-	alert: {
-		text: "",
-		type: "success",
-		active: false,
-	},
-	setAlert: (_) => {},
-	sendAlert: (_) => {},
-	sendError: (_) => {},
-	resetAlert: () => {},
+	alerts: [],
+	activeAlert: null,
+  consumeAlert: () => {},
+  setAlert: (_) => {},
+  sendAlert: (_) => {},
+  sendError: (_) => {},
+  resetAlert: () => {},
+};
+
+export const AlertContext = createContext(initialAlertValue);
+
+import React from "react";
+
+export default function AlertProvider({ children }) {
+  const [alerts, setAlert] = useState([]);
+	const [activeAlert, setActiveAlert] = useState(null);
+	const { addToast } = useToasts()
+
+  const resetAlert = () => {
+    setAlert([]);
+  };
+
+  const sendAlert = (text) => {
+		addToast(text, {appearance: "success", autoDismiss: true})
+		// setAlert([
+    //   ...alerts,
+    //   {
+    //     text,
+    //     type: "success",
+    //     active: true,
+    //   },
+    // ]);
+  };
+
+  const sendError = (text) => {
+		addToast(text, {appearance: "success", autoDismiss: true})
+    // setAlert([
+    //   ...alerts,
+    //   {
+    //     text,
+    //     type: "error",
+    //     active: true,
+    //   },
+    // ]);
+  };
+
+  const consumeAlert = () => {
+    if (alerts.length === 0) {
+			setActiveAlert(null)
+		};
+    const consumed = alerts[0];
+    setAlert(alerts.slice(1));
+    setActiveAlert(consumeAlert);
+  };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        activeAlert,
+        alerts,
+        setAlert,
+        sendAlert,
+        sendError,
+        resetAlert,
+        consumeAlert,
+      }}
+    >
+      {children}
+    </AlertContext.Provider>
+  );
 }
-
-export const AlertContext = createContext(initialAlertValue)
-
-import React from 'react'
-
-export default function AlertProvider({children}) {
-	
-	const [alert, setAlert] = useState({
-		text: "",
-		type: "success",
-		active: false,
-	})
-
-	const resetAlert = () => {
-		setAlert({
-			text: "",
-			type: "success",
-			active: false,
-		})
-	}
-
-	const sendAlert = (text) => {
-		setAlert({
-			text: text,
-			type: "success",
-			active: true,
-		})
-	}
-
-	const sendError = (text) => {
-		setAlert({
-			text: text,
-			type: "error",
-			active: true,
-		})
-	}
-
-	return (
-		<AlertContext.Provider value={{alert, setAlert, sendAlert, sendError, resetAlert}}>
-		{children}
-		</AlertContext.Provider>
-	)
-}
-
 
 export const useAlert = () => {
-	const {alert, resetAlert, sendAlert, sendError} =  useContext(AlertContext)	
-	return {
-		alert,
-		resetAlert,
-		sendAlert,
-		sendError
+  const { activeAlert, alerts, resetAlert, sendAlert, sendError, consumeAlert } = useContext(
+    AlertContext,
+  );
+  return {
+    activeAlert,
+    alerts,
+    consumeAlert,
+    resetAlert,
+    sendAlert,
+    sendError,
   };
 };
