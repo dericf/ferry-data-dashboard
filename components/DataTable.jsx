54ff8116dc5b33c24bd073c5e506ad4bc6db6ecb
deric92 <dericgf@gmail.com>
2021-02-02 22:06:36 -0700

    Convert DataTable to use the main data context

--- components/DataTable.jsx
+++ components/DataTable.jsx
@@ -1,73 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useData } from "../hooks/useData";
 import { dateFormat, timeFormat } from "../utilities/dates";
 import CapacityDataPointInfo from "./CapacityDataPointInfo";
 import Divider from "./Divider";
 import InfoModal from "./InfoModal";
 
-const DataTable = ({ tableData }) => {
-  if (!tableData || tableData?.length == 0) {
-    return <h2 className="text-center">0 Records to show</h2>;
+const DataTable = () => {
+  const { data, refreshData } = useData();
+  useEffect(() => {
+    (async () => {
+      refreshData();
+      // refreshTerminals();
+    })();
+  }, []);
+
+  if (!data || data?.length == 0) {
+    return <h2 className="text-center">0 Records to show for the given filters</h2>;
   }
   // else
   return (
-    <div style={{ overflowX: "auto", maxWidth: "95vw" }}>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th colSpan="2">Crossing Name</th>
-            <th colSpan="2">Date/Time of Sailing</th>
-            <th className="center">Capacity (Available)</th>
-            <th colSpan="2">Date/Time Recorded</th>
-            <th className="center">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((row) => (
-            <tr key={row.id}>
-              <td className="right">{row.id}</td>
-              <td className="center">
-                
-                <div className="flex flex-row justify-center">
-                  <span>{String(row.crossing_name).split("-")[0]}</span>
-                </div>
-              </td>
-              <td className="center">
-              <div className="flex flex-row justify-center">
-                  <span>{String(row.crossing_name).split("-")[1]}</span>
-                </div>
-              </td>
-              <td className="right" style={{ borderRight: "none" }}>
-                {dateFormat(row.date_of_sailing)}
-              </td>
-              <td className="right" style={{ borderLeft: "none" }}>
-                {timeFormat(row.date_of_sailing + " " + row.time_of_sailing)}
-              </td>
-              <td className="center">{row.percent_available} %</td>
-              <td className="right">{dateFormat(row.date_recorded)}</td>
-              <td className="right">
-                {timeFormat(row.date_recorded + " " + row.time_recorded)}{" "}
-              </td>
-              <td className="right">
-                <div
-                  className="flex flex-row justify-center align-center"
-                  style={{ width: "6rem" }}
-                >
-                  <InfoModal
-                    triggerBackgroundColor="primary"
-                    titleText={"Individual Datapoint Information"}
-                    triggerText="Info"
-                    disabled={false}
-                  >
-                    <CapacityDataPointInfo datapoint={row} />
-                  </InfoModal>
-                </div>
-              </td>
+    <>
+      {/* <div className="flex flex-row justify-evenly align-center flex-wrap my-2">
+        {data && (
+          <p className="text-left px-2">
+            Note: This table only displays the 10 newest entries. See buttons
+            above to download entire dataset.
+          </p>
+        )}
+      </div> */}
+      <div style={{ overflowX: "auto", maxWidth: "95vw" }}>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th colSpan="2">Crossing Name</th>
+              <th colSpan="2">Date/Time of Sailing</th>
+              <th className="center">Capacity (Available)</th>
+              <th colSpan="2">Date/Time Recorded</th>
+              <th className="center">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {data.map((row) => (
+              <tr key={row.id}>
+                <td className="right">{row.id}</td>
+                <td className="center">
+                  <div className="flex flex-row justify-center">
+                    <span>{String(row.crossing_name).split("-")[0]}</span>
+                  </div>
+                </td>
+                <td className="center">
+                  <div className="flex flex-row justify-center">
+                    <span>{String(row.crossing_name).split("-")[1]}</span>
+                  </div>
+                </td>
+                <td className="right" style={{ borderRight: "none" }}>
+                  {dateFormat(row.date_of_sailing)}
+                </td>
+                <td className="right" style={{ borderLeft: "none" }}>
+                  {timeFormat(row.date_of_sailing + " " + row.time_of_sailing)}
+                </td>
+                <td className="center">{row.percent_available} %</td>
+                <td className="right">{dateFormat(row.date_recorded)}</td>
+                <td className="right">
+                  {timeFormat(row.date_recorded + " " + row.time_recorded)}{" "}
+                </td>
+                <td className="right">
+                  <div
+                    className="flex flex-row justify-center align-center"
+                    style={{ width: "6rem" }}
+                  >
+                    <InfoModal
+                      triggerBackgroundColor="primary"
+                      titleText={"Individual Datapoint Information"}
+                      triggerText="Info"
+                      disabled={false}
+                    >
+                      <CapacityDataPointInfo datapoint={row} />
+                    </InfoModal>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
