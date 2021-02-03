1a63ef1461ba52d413f98b61d82d48d8f4a939f4
deric92 <dericgf@gmail.com>
2021-02-02 21:48:17 -0700

    Add a reusable main page title component

---
+++ components/Headings/MainTitle.jsx
@@ -0,0 +1,9 @@
+export const MainTitle = () => {
+  return (
+    <div className="text-center flex-shrink flex-no-grow flex-wrap">
+      <h1 className="text-center">
+        Ferry Capacity Dashboard
+      </h1>
+    </div>
+  );
+};
