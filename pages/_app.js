5d0a871a36c6642d904ddcba0435943207a98a63
deric92 <dericgf@gmail.com>
2021-02-17 22:10:46 -0700

    Remove non-tailwind css file imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 import "../styles/globals.css";
 import "../styles/tables.css";
-import "../styles/buttons.css";
-import "../styles/modals.css";
-import "../styles/typography.css";
-import "../styles/alerts.css";
-import "../styles/forms.css";
-import "../styles/layout.css";
-import "../styles/utility.css";
+
 import AlertProvider from "../hooks/useAlert";
 import BasicAuthProvider, { useBasicAuth } from "../hooks/useBasicAuth";
 import LoadingProvider, { useIsLoading } from "../hooks/useIsLoading";
@@ -15,19 +9,17 @@ import { ToastProvider, useToasts } from "react-toast-notifications";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className="container text-white flex flex-col align-items-center">
-      <ToastProvider>
-        <AlertProvider>
-          <LoadingProvider>
-            <BasicAuthProvider>
-              <DataProvider>
-                <Component {...pageProps} />
-              </DataProvider>
-            </BasicAuthProvider>
-          </LoadingProvider>
-        </AlertProvider>
-      </ToastProvider>
-    </main>
+    <ToastProvider>
+      <AlertProvider>
+        <LoadingProvider>
+          <BasicAuthProvider>
+            <DataProvider>
+              <Component {...pageProps} />
+            </DataProvider>
+          </BasicAuthProvider>
+        </LoadingProvider>
+      </AlertProvider>
+    </ToastProvider>
   );
 }
 
