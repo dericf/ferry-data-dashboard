a1be9785845500483e94676347b6f923618fc7ad
deric92 <dericgf@gmail.com>
2021-02-17 22:20:02 -0700

    Significantly simplify useAlert hook | remove unneeded context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,21 @@
 import "../styles/globals.css";
 import "../styles/tables.css";
 
-import AlertProvider from "../hooks/useAlert";
 import BasicAuthProvider, { useBasicAuth } from "../hooks/useBasicAuth";
 import LoadingProvider, { useIsLoading } from "../hooks/useIsLoading";
 import DataProvider from "../hooks/useData";
-import { ToastProvider, useToasts } from "react-toast-notifications";
+import { ToastProvider } from "react-toast-notifications";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ToastProvider>
-      <AlertProvider>
-        <LoadingProvider>
-          <BasicAuthProvider>
-            <DataProvider>
-              <Component {...pageProps} />
-            </DataProvider>
-          </BasicAuthProvider>
-        </LoadingProvider>
-      </AlertProvider>
+      <LoadingProvider>
+        <BasicAuthProvider>
+          <DataProvider>
+            <Component {...pageProps} />
+          </DataProvider>
+        </BasicAuthProvider>
+      </LoadingProvider>
     </ToastProvider>
   );
 }
