b13f6bf98a99d8ab9ed45b07f1cf6c090ac9fb72
deric92 <dericgf@gmail.com>
2021-02-17 22:14:41 -0700

    Convert DataTable component to tailwind classes Still need to abstract a generic Table Component

--- components/DataTable.jsx
+++ components/DataTable.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 import { useData } from "../hooks/useData";
+import getFilteredData from "../pages/api/get-filtered-data";
 import { dateFormat, timeFormat } from "../utilities/dates";
 import CapacityDataPointInfo from "./CapacityDataPointInfo";
 import Divider from "./Divider";
 import InfoModal from "./InfoModal";
 
 const DataTable = () => {
-  const { data, refreshData } = useData();
+  const { data, refreshData, getLimitedData } = useData();
   useEffect(() => {
     (async () => {
-      refreshData();
-      // refreshTerminals();
+      await getLimitedData()
     })();
   }, []);
 
@@ -28,44 +28,44 @@ const DataTable = () => {
           </p>
         )}
       </div> */}
-      <div style={{ overflowX: "auto", maxWidth: "95vw" }}>
-        <table>
-          <thead>
+      <div lassName="shadow mx-auto w-screen max-w-2xl px-4 py-2 overflow-x-auto border-b border-gray-200 sm:rounded-lg">
+        <table className="min-w-full divide-y divide-gray-200 table-auto">
+          <thead className="bg-gray-50">
             <tr>
-              <th>Id</th>
-              <th colSpan="2">Crossing Name</th>
-              <th colSpan="2">Date/Time of Sailing</th>
-              <th className="center">Capacity (Available)</th>
-              <th colSpan="2">Date/Time Recorded</th>
-              <th className="center">Actions</th>
+              <th className="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Id</th>
+              <th className="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"  colSpan="2">Crossing Name</th>
+              <th className="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider" colSpan="2">Date/Time of Sailing</th>
+              <th className="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Capacity (Available)</th>
+              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider" colSpan="2">Date/Time Recorded</th>
+              <th className="text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
             </tr>
           </thead>
-          <tbody>
-            {data.map((row) => (
+          <tbody class="bg-white divide-y divide-gray-200">
+            {data.slice(0, 10).map((row, index) => (
               <tr key={row.id}>
-                <td className="right">{row.id}</td>
-                <td className="center">
+                <td className="text-sm text-gray-900 text-right">{row.id}</td>
+                <td className="text-sm text-gray-900 text-center">
                   <div className="flex flex-row justify-center">
                     <span>{String(row.crossing_name).split("-")[0]}</span>
                   </div>
                 </td>
-                <td className="center">
+                <td className="text-sm text-gray-900 text-center">
                   <div className="flex flex-row justify-center">
                     <span>{String(row.crossing_name).split("-")[1]}</span>
                   </div>
                 </td>
-                <td className="right" style={{ borderRight: "none" }}>
+                <td className="text-sm text-gray-900 text-right" style={{ borderRight: "none" }}>
                   {dateFormat(row.date_of_sailing)}
                 </td>
-                <td className="right" style={{ borderLeft: "none" }}>
+                <td className="text-sm text-gray-900 text-right" style={{ borderLeft: "none" }}>
                   {timeFormat(row.date_of_sailing + " " + row.time_of_sailing)}
                 </td>
-                <td className="center">{row.percent_available} %</td>
-                <td className="right">{dateFormat(row.date_recorded)}</td>
-                <td className="right">
+                <td className="text-sm text-gray-900 text-center">{row.percent_available} %</td>
+                <td className="text-sm text-gray-900 text-right">{dateFormat(row.date_recorded)}</td>
+                <td className="text-sm text-gray-900 text-right">
                   {timeFormat(row.date_recorded + " " + row.time_recorded)}{" "}
                 </td>
-                <td className="right">
+                <td className="text-sm text-gray-900 text-right">
                   <div
                     className="flex flex-row justify-center align-center"
                     style={{ width: "6rem" }}
