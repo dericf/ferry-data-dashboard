1b8f29b4e4e82d87ac7e160e322b480092ef5ab1
deric92 <dericgf@gmail.com>
2021-02-17 22:08:41 -0700

    Fix MainTitle component

--- components/Headings/MainTitle.jsx
+++ components/Headings/MainTitle.jsx
@@ -1,9 +1,3 @@
 export const MainTitle = () => {
-  return (
-    <div className="text-center flex-shrink flex-no-grow flex-wrap">
-      <h1 className="text-center">
-        Ferry Capacity Dashboard
-      </h1>
-    </div>
-  );
+  return <h1 className="text-center text-4xl">Ferry Capacity Dashboard</h1>;
 };
