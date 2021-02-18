131fa929000564638354a4bb61276d10ecab7534
deric92 <dericgf@gmail.com>
2021-02-17 22:08:08 -0700

    Fix logout button

--- components/Auth/LogoutButton.jsx
+++ components/Auth/LogoutButton.jsx
@@ -1,18 +1,18 @@
 import { useBasicAuth } from "../../hooks/useBasicAuth";
 import { usePingServer } from "../../hooks/usePingServer";
+import { Button } from "../Button";
 
 export const LogoutButton = () => {
   const { logout } = useBasicAuth();
 
   return (
     <div
-      className="flex flex-col justify-end px-2 my-2"
-			style={{position: "fixed", top: 0, left: 0}}
+      className="flex flex-col justify-end px-2 my-2 fixed top-2 right-2 z-10"
     >
       {/* <span className="alert success text-small text-center">
         Authenticated
       </span> */}
-      <button onClick={logout}>Logout</button>
+      <Button color="white" onClick={logout}>Logout</Button>
     </div>
   );
 };
