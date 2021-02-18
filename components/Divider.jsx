5a303f82eae53d7eaaaa9d0f736a7fc8c4194e47
deric92 <dericgf@gmail.com>
2021-02-17 22:09:19 -0700

    Convert Divider comonent to tailwind

--- components/Divider.jsx
+++ components/Divider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Divider(props) {
 	return (
-		<div {...props} className={"divider " + props?.className}>
+		<div {...props} className={"border-b-2 border-solid w-10/12 mx-auto my-4 h-4 " + props?.className}>
 
 		</div>
 	)
