f4a1179355dc2624664ccdb6bf513eb790fe1674
deric92 <dericgf@gmail.com>
2021-02-24 20:44:19 -0700

    Return demo user state from authenticate api route

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
-  
-	console.log();
-	if (req.body?.password === process.env.DASHBOARD_PASSWORD || req.body?.password === process.env.DEMO_DASHBOARD_PASSWORD) {
-		res.statusCode = 200
-	}
-	else {
-		res.statusCode = 401
-	}	
-  res.json({})
-}
+  console.log();
+  if (
+    req.body?.password === process.env.DASHBOARD_PASSWORD ||
+    req.body?.password === process.env.DEMO_DASHBOARD_PASSWORD
+  ) {
+    res.statusCode = 200;
+  } else {
+    res.statusCode = 401;
+  }
+  res.json({
+    isDemoUser: req.body?.password === process.env.DEMO_DASHBOARD_PASSWORD,
+  });
+};
