00bdeca73cb7a822715314f85727f9e86801c214
deric92 <dericgf@gmail.com>
2021-03-01 22:31:17 -0700

    Convert Dashboard to .tsx

--- components/DashBoard/DashBoard.tsx
+++ components/DashBoard/DashBoard.tsx
@@ -17,11 +17,11 @@ import { useData } from "../../hooks/useData";
 import LoadingBackdrop from "../LoadingBackdrop";
 import { LogoutButton } from "../Auth/LogoutButton";
 import { DataVisualizer } from "./DataVisualizer";
-import { Filter } from "./Filter";
 import { useIsLoading } from "../../hooks/useIsLoading";
 import dynamic from "next/dynamic";
 import { LoadingText } from "../Loading/LoadingText";
 // import { DataVisualizer from "./DataVisualizer
+import { MainTitle } from "@/components/Headings/MainTitle";
 
 const DynamicLineChart = dynamic(
   () => import("./Charts/LineChart").then((mod) => mod.LineChart),
@@ -33,17 +33,11 @@ export default function Dashboard() {
   const { loadingState, setLoadingState } = useIsLoading();
 
   const [open, toggle] = useToggle(null);
-  const { isAuthenticated, pwd } = useBasicAuth();
-
+  const { isAuthenticated, pwd, isDemoUser } = useBasicAuth();
+  
   const { resetAlert, sendAlert, sendError } = useAlert();
 
   // let formatted_date_string = new Date().toUTCString().replace(/ /g, "_").replace(/,/g, "").replace(/:/g, "_")
-
-  // useEffect(() => {
-  //   if (terminals) {
-  //     setSelectedTerminal(terminals[0]);
-  //   }
-  // }, [terminals]);
 
   return (
     <>
@@ -52,18 +46,12 @@ export default function Dashboard() {
         <link key="favicon" rel="icon" href="/favicon.ico" />
       </Head>
       <LogoutButton />
-
+      <MainTitle>Ferry Data Dashboard {isDemoUser === true ? "(Demo)" : ""}</MainTitle>
       <div className="flex flex-row flex-shrink flex-no-grow justify-center align-end flex-wrap shadow-md py-4">
         <WorkerPortal></WorkerPortal>
       </div>
 
       <div className="flex flex-col align-middle px-4 mt-4  " style={{ width: "95vw" }}>
-        {/* <Filter /> */}
-        {/* {data && data.length > 0 && (
-          <div className={"mb-3"}>
-            <DynamicLineChart />
-          </div>
-        )} */}
         <DataTable></DataTable>
       </div>
     </>
